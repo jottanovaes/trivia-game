3763201b7eff1e4b5b3ed9d3e07874b018da25b2
jottanovaes <jottanovaes@gmail.com>
2022-02-14 21:17:38 -0300

    feat: settings button

---
+++ src/components/SettingsBtn/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FcSettings } from 'react-icons/fc';
+import PropTypes from 'prop-types';
+
+export default class SettingsBtn extends React.Component {
+  render() {
+    const { history } = this.props;
+    return (
+      <FcSettings
+        data-testid="btn-settings"
+        onClick={ () => history.push('/settings') }
+        style={ { cursor: 'pointer', marginLeft: '8px', width: '30px', height: '30px' } }
+      />
+    );
+  }
+}
+
+SettingsBtn.propTypes = {
+  history: PropTypes.objectOf,
+}.isRequired;
