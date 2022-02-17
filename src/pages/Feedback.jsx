0ea158843e9d18d7cfa24b0b00c786f24567e56b
MarcsVinicius <marcos.vgalencar@gmail.com>
2022-02-16 21:04:04 -0300

    req15 part1

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
+import { requestSetPlayer } from '../actions';
 
 class Feedback extends React.Component {
+  handleClick = () => {
+    const { payload } = this.props;
+    payload({ score: 0, assertions: 0 });
+  }
+
   render() {
     const { score } = this.props;
     const getLocalStorage = (localStorage.getItem('state') !== null)
@@ -24,6 +32,16 @@ class Feedback extends React.Component {
         <section>
           <h2 data-testid="feedback-total-question">{assertions}</h2>
         </section>
+        <Link to="/">
+          <button
+            type="button"
+            onClick={ this.handleClick }
+            data-testid="btn-play-again"
+          >
+            Play Again
+
+          </button>
+        </Link>
       </div>
     );
   }
@@ -33,4 +51,9 @@ Feedback.propTypes = {
   score: PropTypes.number,
 }.isRequired;
 
-export default Feedback;
+const mapDispatchToProps = (dispatch) => ({
+
+  payload: (payload) => dispatch(requestSetPlayer(payload)),
+
+});
+export default connect(null, mapDispatchToProps)(Feedback);
