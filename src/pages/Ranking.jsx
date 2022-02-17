acce72ed3248830cf2210a46243cc39f459b224f
gemaquejr <gemaquejr@hotmail.com>
2022-02-17 02:17:34 -0300

    Corrige  requisito 17

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
   render() {
+    const { history } = this.props;
     const getLocalStorage = JSON.parse(localStorage.getItem('ranking'));
-    const playerRanking = getLocalStorage.sort((a, b) => b.score - a.score);
+    const playerRanking = getLocalStorage
+      && getLocalStorage.sort((a, b) => b.score - a.score);
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
         <div>
-          { playerRanking.map((player, index) => (
+          {playerRanking && playerRanking.map((player, index) => (
             <div key={ index }>
               <img src={ player.gravatarEmail } alt={ player.name } />
-              <p data-testid={ `player-name-${index}` }>{ player.name }</p>
-              <p data-testid={ `player-score-${index}` }>{ player.score }</p>
+              <p data-testid={ `player-name-${index}` }>{player.name}</p>
+              <p data-testid={ `player-score-${index}` }>{player.score}</p>
             </div>
           ))}
         </div>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Voltar ao Início
+
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.objectOf,
+}.isRequired;
 
 export default Ranking;
