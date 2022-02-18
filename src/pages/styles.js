c0f7307d088e85e48dea6c4d0dc093b629d1b3cc
jottanovaes <jottanovaes@gmail.com>
2022-02-17 23:16:00 -0300

    style: aplly glowing neon effect at the input field and change settings button position

--- src/pages/styles.js
+++ src/pages/styles.js
@@ -1,8 +1,85 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const StyledLoginField = styled.div`
+export const StyledLoginField = styled.div`
+  align-items: center;
   display: flex;
-  flex-grow: column;
+  flex-direction: column;
+  gap: 2vh;
+  padding-top: 2vh;
 `;
 
-export default StyledLoginField;
+export const StyledInput = styled.input`
+  background-color: transparent;
+  border-radius: 4px;
+  box-shadow: 0 22px 56px -36px rgba(255, 255, 255, 0.5),
+    0 4px 5px -4px rgba(255, 255, 255, 1),
+    0 -31px 34px -32px rgba(96, 68, 144, 0.3),
+    0 39px 50px -34px rgba(202, 172, 255, 0.3),
+    0 2px 9px rgba(154, 146, 210, 0.3),
+    0 1px 10px rgba(227, 222, 255, 0.2);
+  border: none;
+  color: white;
+  font-family: 'Roboto', sans-serif;
+  font-size: 2.5vh;
+  font-weight: 700;
+  height: 42px;
+  outline: none;
+  text-indent: 12px;
+  width: 25vw;
+
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.5);
+  }
+`;
+
+const ButtonAnimation = keyframes`
+  from { box-shadow: 0 0 0.5vw #2fc18c, 0 0 1vw #2fc18c, 0 0 1.5vw #2fc18c; }
+  to { box-shadow: 0 0 1.5vw #3effb8,0 0 1vw #2fc18c, 0 0 0.5vw #2fc18c; }
+`;
+
+export const StyledButton = styled.button`
+  background-color: transparent;
+  border-radius: 4px;
+  border: none;
+  color: rgba(255, 255, 255, 0.5);
+  font-family: 'Roboto', sans-serif;
+  font-size: 2.5vh;
+  font-weight: 700;
+  height: 42px;
+  outline: none;
+  width: 25vw;
+
+  &:enabled {
+    animation: ${ButtonAnimation} 1s ease-in-out infinite alternate;
+    background-color: #2fc18c;
+    box-shadow: 0 0 3vw #3effb8;
+    color: white;
+    cursor: pointer;
+  }
+`;
+
+// https://www.cssscript.com/demo/neon-glass-cards/
+export const StyledInputContainer = styled.div`
+  background: rgba(255, 255, 255, 0.01);
+  border-radius: 8px;
+  box-shadow: inset 0 22px 56px -36px rgba(255, 255, 255, 0.5),
+    inset 0 4px 5px -4px rgba(255, 255, 255, 1),
+    inset 0 -31px 34px -32px rgba(96, 68, 144, 0.3),
+    inset 0 39px 50px -34px rgba(202, 172, 255, 0.3),
+    inset 0 2px 9px rgba(154, 146, 210, 0.3),
+    inset 0 1px 10px rgba(227, 222, 255, 0.2);
+  color: #fff;
+  margin: auto;
+  padding: 4vh;
+  transition: 0.3s;
+  width: 33%;
+
+  &:hover {
+    box-shadow: inset 0 19px 28px -18px rgba(255, 255, 255, 0.5),
+      inset 0 4px 6px -3px rgba(255, 255, 255, 1),
+      inset 0 -51px 44px -42px rgba(96, 68, 144, 0.3),
+      inset 0 59px 60px -32px rgba(202, 172, 255, 0.3),
+      inset 0 4px 16px rgba(154, 146, 210, 0.3),
+      inset 0 2px 25px rgba(227, 222, 255, 0.23);
+  }
+`;
