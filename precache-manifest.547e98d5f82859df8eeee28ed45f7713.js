b8e5efd2ad07f1fa7ed3c2e70bb792f2afc618a3
jottanovaes <jottanovaes@gmail.com>
2022-02-23 02:18:50 -0300

    Updates

---
+++ precache-manifest.547e98d5f82859df8eeee28ed45f7713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cea3519e380ed8a6261cb63f92dedc8",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "4495311e6e885e66c5c4",
+    "url": "/trivia-game/static/css/main.f921894d.chunk.css"
+  },
+  {
+    "revision": "9a3bb8ccf4986dda64f5",
+    "url": "/trivia-game/static/js/2.e497a0df.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/trivia-game/static/js/2.e497a0df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4495311e6e885e66c5c4",
+    "url": "/trivia-game/static/js/main.a73a2a2c.chunk.js"
+  },
+  {
+    "revision": "b0c6c90c513f51408a18",
+    "url": "/trivia-game/static/js/runtime-main.4bc9de50.js"
+  }
+]);
